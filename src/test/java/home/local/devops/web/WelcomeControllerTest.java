307546619ec41627b27a730ee5d65ce4692c8e32
tej_gill24@yahoo.com <tej_gill24@yahoo.com>
2020-09-24 12:30:00 -0400

    change to orignal mapping

--- src/test/java/home/local/devops/web/WelcomeControllerTest.java
+++ src/test/java/home/local/devops/web/WelcomeControllerTest.java
@@ -32,7 +32,7 @@ class WelcomeControllerTest {
 
 	@Test
 	void testWelcome() throws Exception {
-		mockMvc.perform(get("/devops/")).andExpect(status().isOk())
+		mockMvc.perform(get("/")).andExpect(status().isOk())
 		.andExpect(model().attribute("course", containsString("Devops")));
 
 	}
